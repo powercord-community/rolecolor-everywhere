c027c87df5530058b6f2955c858938f37b2f965d
Kal Sandbrook <kal.sandbrook1@gmail.com>
2021-02-05 08:33:52 +0000

    Grammar and English refinements (#17)

--- Settings.jsx
+++ Settings.jsx
@@ -11,63 +11,63 @@ module.exports = class Settings extends React.PureComponent {
     return (
       <div>
         <SwitchItem
-          note='Should your username in account box indicator be colored'
+          note='Whether your username is colored within the account box indicator'
           value={this.props.getSetting('account', true)}
           onChange={() => this.props.toggleSetting('account', true)}
         >
           {Messages.ACCOUNT}
         </SwitchItem>
         <SwitchItem
-          note='Whether hould usernames in voice channels be colored or not'
+          note='Whether usernames in voice channels are colored'
           value={this.props.getSetting('voice', true)}
           onChange={() => this.props.toggleSetting('voice', true)}
         >
           Voice users
         </SwitchItem>
         <SwitchItem
-          note='Whether hould mentions in chat be colored or not'
+          note='Whether mentions in chat are colored'
           value={this.props.getSetting('mentions', true)}
           onChange={() => this.props.toggleSetting('mentions', true)}
         >
           {Messages.FORM_LABEL_MENTIONS}
         </SwitchItem>
         <SwitchItem
-          note='Whether hould typing indicator be colored or not'
+          note='Whether typing indicators are colored'
           value={this.props.getSetting('typing', true)}
           onChange={() => this.props.toggleSetting('typing', true)}
         >
           Typing indicator
         </SwitchItem>
         <SwitchItem
-          note='Whether hould role names in member list be colored or not'
+          note='Whether role names in the member list are colored'
           value={this.props.getSetting('members', true)}
           onChange={() => this.props.toggleSetting('members', true)}
         >
           Members list
         </SwitchItem>
         <SwitchItem
-          note='Whether hould user statuses in member list be colored or not'
+          note='Whether user statuses in the member list are colored'
           value={this.props.getSetting('status', true)}
           onChange={() => this.props.toggleSetting('status', true)}
         >
           {Messages.FRIENDS_COLUMN_STATUS}
         </SwitchItem>
         <SwitchItem
-          note='Whether hould messages be colored or not'
+          note='Whether messages are colored'
           value={this.props.getSetting('messages', true)}
           onChange={() => this.props.toggleSetting('messages', true)}
         >
           {Messages.MESSAGES}
         </SwitchItem>
         <SwitchItem
-          note='Whether hould system messages be colored. or not'
+          note='Whether system messages are colored'
           value={this.props.getSetting('systemMessages', true)}
           onChange={() => this.props.toggleSetting('systemMessages', true)}
         >
           System messages
         </SwitchItem>
         <SwitchItem
-          note='Whether hould names in user popouts be colored. or not'
+          note='Whether usernames in user popouts are colored'
           value={this.props.getSetting('userPopouts', true)}
           onChange={() => this.props.toggleSetting('userPopouts', true)}
         >
